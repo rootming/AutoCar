c41ad1c586fa6989d2dc3897a30cb425883821d2
rootming <rootming@live.cn>
2016-07-30 22:35:36 +0800

    merge config

--- linux/src/config/config.h
+++ linux/src/config/config.h
@@ -1,11 +1,13 @@
 #pragma once
 
+
+//WiringPi PIN
 #define _SR1_TRIG_PIN           0
 #define _SR1_ECHO_PIN           1
 #define _SR2_TRIG_PIN           2
 #define _SR2_ECHO_PIN           3
-#define _SR3_TRIG_PIN           4
-#define _SR3_ECHO_PIN           5
+// #define _SR3_TRIG_PIN           4
+// #define _SR3_ECHO_PIN           5
 
 #define _MOTOR_LEFT_PIN         6
 #define _MOTOR_RIGHT_PIN        7
