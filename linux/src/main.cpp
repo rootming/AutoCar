97262e75ea345472ea7f195c23b55fb1665b69ce
rootming <rootming@live.cn>
2016-07-30 22:27:47 +0800

    fix pin

--- linux/src/main.cpp
+++ linux/src/main.cpp
@@ -7,16 +7,17 @@ using namespace std;
 
 int main()
 {
-    UltraSonic test2;
+//    UltraSonic test2;
     Motor motor(0, 255);
-    test2.setPin(0, 1);
-    test2.autoScan();
-    motor.setPin(6, 7, 21, 22, 23, 24);
+  //  test2.setPin(0, 1);
+  //  test2.autoScan();
+    motor.setPin(2, 3, 22, 23, 24, 25);
     motor.setLeftSpeed(100);
     motor.setRightSpeed(100);
-    for(;;){
-        cout << test2.getDistance() << endl;
-        sleep(1);
-    }
+ //   for(;;){
+ //       cout << test2.getDistance() << endl;
+ //       sleep(1);
+ //   }
+    cin.get();
     return 0;
 }
