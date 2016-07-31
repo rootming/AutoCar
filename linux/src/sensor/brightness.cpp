3f9a332370c12e13615d8fde3024dceeb64b381a
rootming <rootming@live.cn>
2016-08-01 01:02:35 +0800

    fix bug

--- linux/src/sensor/brightness.cpp
+++ linux/src/sensor/brightness.cpp
@@ -9,10 +9,10 @@
 Brightness::Brightness()
 {
     pcf8591Setup(_BG_PIN, _ADC_ADDRESS);
-    pinMode(_BG_PIN, INPUT);
+    pinMode(_BG_PIN + 1, INPUT);
 }
 
 int Brightness::getValue(void)
 {
-    return analogRead(_BG_PIN);
+    return analogRead(_BG_PIN + 1);
 }
